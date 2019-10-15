da6a4383fdf28c38b2c551fd729aec79213dcba5
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-10-15 12:17:41 +0900

    added create function

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,19 +1,30 @@
 class ApartmentsController < ApplicationController
   def index
+    @apartments = Apartment.all
   end
 
   def new
     @apartment = Apartment.new
 
+
   end
 
   def create
+    @apartment = Apartment.create(apartment_params)
 
+    redirect_to apartments_path
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+
+  def apartment_params
+    params.require(:apartment).permit(:name,:price,:address,:year,:note)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :apartment
+  resources :apartments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
